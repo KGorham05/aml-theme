deee0eb8615717b047bdb2e2e04c5fac8ef16ef0
Kgor <kevin.gorham@gmail.com>
2019-06-26 15:59:02 -0700

    created php files

---
+++ footer.php
@@ -0,0 +1,62 @@
+<?php?>
+
+<!-- footer -->
+<div id="plant-foot-img"></div>
+    <div id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2">
+                    <h3>Shop</h3>
+                    <a class="foot-link" href="#">All</a><br>
+                    <a class="foot-link" href="#">Stabilizers</a><br>
+                    <a class="foot-link" href="#">Cartridges</a><br>
+                    <a class="foot-link" href="#">Terpenes</a><br>
+                    <a class="foot-link" href="#">Equipment</a><br>
+                </div>
+                <div class="col-md-3">
+                    <h3>Explore</h3>
+                    <a class="foot-link" href="#">About</a><br>
+                    <a class="foot-link" href="#">Terpology 101</a><br>
+                    <a class="foot-link" href="#">Shipping Policy</a><br>
+                    <a class="foot-link" href="#">Returns</a><br>
+                    <a class="foot-link" href="#">Privacy Policy</a><br>
+                    <a class="foot-link" href="#">Terms of Use</a><br>
+                </div>
+                <div class="col-md-3">
+                    <h3>Contact</h3>
+                    Email: <br>
+                    <a class="foot-link" href="#">info@aftermathlab.com</a>
+                </div>
+                <div class="col-md-4">
+                    <h3>Sign Up For Our Newsletter!</h3>
+                    <form id="email-form" class=>
+                        <input type="text" placeholder="EMAIL ADDRESS" id="email-input">
+                        <button id="email-btn">></button>
+                    </form>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6 text-center" id="copy">Copyright © 2019 Aftermath Lab. All Rights Reserved</div>
+                <div class="col-md-2"></div>
+                <div class="col-md-1">
+                    <a href="#">
+                        <img src="http://v2.aftermathlab.com/wp-content/uploads/2019/06/back-to-top-button.png"
+                            height="40" width="40">
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+
+
+    
+</body>
+
+</html>
+
---
+++ index.php
@@ -0,0 +1,117 @@
+    <?php get_header(); ?>
+    
+    <!-- Main page content -->
+    <div class="container-fluid">
+        <!-- hero row -->
+        <div class="row">
+            <div id="hero-div" class="text-center">
+                <img src="http://v2.aftermathlab.com/wp-content/uploads/2019/06/logo-flask-transparent-opt-hero.png"
+                    width=65 id="hero-flask-logo">
+                <h1 id="hero-title">CLEAN CUT</h1>
+                <h2 id="hero-tag-line">Born from obsession of achieving the perfect balance of aroma and flavor</h2>
+                <button id="hero-btn">SHOP NOW</button>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div id="stab-div" class="text-center">
+                    <button class="module cat-btn" id="stab-btn">
+                        <div class="module__top">
+                            <div class="top__content">
+                                <img src="http://v2.aftermathlab.com/wp-content/uploads/2019/06/plant-icon-white-transparent2-1.png"
+                                    height="20" width="20">
+                            </div>
+                        </div>
+                        <div class="module__contents">LAB SUPPLY</div>
+                    </button>
+                </div>
+                <div id="cart-div" class="text-center">
+                    <button class="module cat-btn" id="cart-btn">
+                        <div class="module__top">
+                            <div class="top__content">
+                                <img src="http://v2.aftermathlab.com/wp-content/uploads/2019/06/plant-icon-white-transparent2-1.png"
+                                    height="20" width="20">
+                            </div>
+                        </div>
+                        <div class="module__contents">CARTRIDGES</div>
+                    </button>
+                </div>
+                <div id="terp-div" class="text-center">
+                    <button class="module cat-btn" id="terp-btn">
+                            <div class="module__top">
+                                <div class="top__content">
+                                    <img src="http://v2.aftermathlab.com/wp-content/uploads/2019/06/plant-icon-white-transparent2-1.png"
+                                        height="20" width="20">
+                                </div>
+                            </div>
+                            <div class="module__contents">TERPENES</div>
+                        </button>
+                </div>
+                <div id="equip-div" class="text-center">
+                    <!-- <button id="equip-btn" class="cat-btn">EQUIPMENT</button> -->
+                    <button class="module cat-btn" id="equip-btn">
+                            <div class="module__top">
+                                <div class="top__content">
+                                    <img src="http://v2.aftermathlab.com/wp-content/uploads/2019/06/plant-icon-white-transparent2-1.png"
+                                        height="20" width="20">
+                                </div>
+                            </div>
+                            <div class="module__contents">EQUIPMENT</div>
+                        </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row" id="post-row">
+            <div class="col-md-6 padding-back text-center">
+                <div id="terpology-img"></div>
+                <img class="gold-plant-icon"
+                    src="http://v2.aftermathlab.com/wp-content/uploads/2019/06/plant-icon-gold-transparent.png"
+                    height="25" width="25">
+                <h2 class="post-title">Terpology 101</h2>
+                <p class="post-text">Terpenes are a large and diverse class of organic compounds produced by a variety
+                    of plants. Terpenes are what give each flower, herb and fruit its own unique scent and flavor. Every
+                    day, everywhere you go, you encounter terpenes. Stop and smell the roses? More like stop and smell
+                    the terpenes.
+                </p>
+            </div>
+            <div class="col-md-6 padding-back text-center">
+                <div id="team-img"></div>
+                <img class="gold-plant-icon"
+                    src="http://v2.aftermathlab.com/wp-content/uploads/2019/06/plant-icon-gold-transparent.png"
+                    height="25" width="25">
+                <h2 class="post-title">The Aftermath Team</h2>
+                <p class="post-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, libero incidunt
+                    reiciendis exercitationem tempore quia id odit provident sit at consequuntur? Incidunt dolore, amet
+                    voluptas tempore cumque voluptate vitae inventore.</p>
+            </div>
+        </div>
+
+<?php
+
+    if( have_posts()){
+        while ( have_posts()){
+            the_post();
+        }
+    }
+?>
+
+        <div class="row" id="social-media-row">
+            <div class="col-md-12">
+                <img src="" class="marg-right" height="214" width="214">
+                <img src="" class="marg-right" height="214" width="214">
+                <img src="" class="marg-right" height="214" width="214">
+                <img src="" class="marg-right" height="214" width="214">
+                <img src="" height="214" width="214">
+            </div>
+
+        </div>
+
+
+
+    </div>
+
+    <?php get_footer(); ?>
