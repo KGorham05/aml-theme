a1f97f995e2b15a3012286e0ebc2b139760ea878
Kgor <kevin.gorham@gmail.com>
2019-07-09 09:24:58 -0700

    new morning update

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <!-- Main CSS -->
-    <link type="text/css" media="all" rel="stylesheet" href="style.css">
+    <link type="text/css" media="all" rel="stylesheet" href="aml-style.css">
 
 </head>
 
